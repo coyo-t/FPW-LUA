6060cab554649cd2274f2d032f94d30f1b398b23
The Birthday Coyote :] <constachugga@gmail.com>
2025-01-08 22:43:55 -0800

    s

--- src/libs/bufferlib.cpp
+++ src/libs/bufferlib.cpp
@@ -5,75 +5,100 @@
 #include "llimits.hpp"
 #include "../lua.hpp"
 #include "../lauxlib.hpp"
-#include "../luatemplate.hpp"
 #include <cstring>
 
 namespace CoyoteBuffer {
 
 using Byte = lu_byte;
 
+
+struct Buffer
+{
+	size_t size;
+	size_t cursor;
+	Byte data[];
+
+	static auto createsize (size_t size) -> size_t
+	{
+		return sizeof(Buffer) + (size * sizeof(Byte));
+	}
+};
+
+enum class BufferError
+{
+	NO_ALIEN_OvO,
+	UNDERFLOW,
+	OVERFLOW,
+};
+
+
+
+
+
+
+
 #define COYOTE_BUFFER_REG "GML_BUFFER*"
 
-static Byte* checkbuffer (lua_State* L, int index)
-{
-	return static_cast<Byte*>(luaL_checkudata(L, index, COYOTE_BUFFER_REG));
-}
-
-static lua_Integer buffer_tell (lua_State* L, int i)
-{
-	lua_getiuservalue(L, i, 1);
-	lua_Integer outs = lua_tointeger(L, -1);
-	lua_pop(L, 1);
-	return outs;
-}
-
-static lua_Integer buffer_get_size (lua_State* L, int i)
-{
-	lua_getiuservalue(L, i, 2);
-	lua_Integer outs = lua_tointeger(L, -1);
-	lua_pop(L, 1);
-	return outs;
-}
-
-static int f_create (lua_State* L)
-{
-	lua_Integer count = luaL_checkinteger(L, 1);
-
-	if (count < 0)
-	{
-		return luaL_error(L, "Buffer size %d less than 0", count);
-	}
-
-	auto* f = static_cast<Byte*>(lua_newuserdatauv(L, count, 2));
-	std::memset(f, 0, static_cast<size_t>(count));
-
-	// cursor
-	lua_pushinteger(L, 0);
-	lua_setiuservalue(L, -2, 1);
-
-	// size
-	lua_pushinteger(L, count);
-	lua_setiuservalue(L, -2, 2);
-
-	luaL_setmetatable(L, COYOTE_BUFFER_REG);
-
-	return 1;
-}
-
-
-static int f_read_byte (lua_State* L)
-{
-	auto f = checkbuffer(L, 1);
-
-	auto cursor = buffer_tell(L, 1);
-
-	if (cursor < 0)
-	{
-		return luaL_error(L, "Buffer underflow: %d", cursor);
-	}
-
-	auto size = buffer_get_size(L, 1);
-
+// static Byte* checkbuffer (lua_State* L, int index)
+// {
+// 	return static_cast<Byte*>(luaL_checkudata(L, index, COYOTE_BUFFER_REG));
+// }
+//
+// static lua_Integer buffer_tell (lua_State* L, int i)
+// {
+// 	lua_getiuservalue(L, i, 1);
+// 	lua_Integer outs = lua_tointeger(L, -1);
+// 	lua_pop(L, 1);
+// 	return outs;
+// }
+//
+// static lua_Integer buffer_get_size (lua_State* L, int i)
+// {
+// 	lua_getiuservalue(L, i, 2);
+// 	lua_Integer outs = lua_tointeger(L, -1);
+// 	lua_pop(L, 1);
+// 	return outs;
+// }
+//
+// static int f_create (lua_State* L)
+// {
+// 	lua_Integer count = luaL_checkinteger(L, 1);
+//
+// 	if (count < 0)
+// 	{
+// 		return luaL_error(L, "Buffer size %d less than 0", count);
+// 	}
+//
+// 	auto* f = static_cast<Byte*>(lua_newuserdatauv(L, count, 2));
+// 	std::memset(f, 0, static_cast<size_t>(count));
+//
+// 	// cursor
+// 	lua_pushinteger(L, 0);
+// 	lua_setiuservalue(L, -2, 1);
+//
+// 	// size
+// 	lua_pushinteger(L, count);
+// 	lua_setiuservalue(L, -2, 2);
+//
+// 	luaL_setmetatable(L, COYOTE_BUFFER_REG);
+//
+// 	return 1;
+// }
+//
+//
+// static int f_read_byte (lua_State* L)
+// {
+// 	auto f = checkbuffer(L, 1);
+//
+// 	auto cursor = buffer_tell(L, 1);
+//
+// 	if (cursor < 0)
+// 	{
+// 		return luaL_error(L, "Buffer underflow: %d", cursor);
+// 	}
+//
+// 	auto size = buffer_get_size(L, 1);
+//
 
 }
 
--- src/libs/linit.cpp
+++ src/libs/linit.cpp
@@ -49,7 +49,6 @@ static const luaL_Reg loadedlibs[] = {
 	{LUA_MATHLIBNAME, luaopen_math},
 	{LUA_UTF8LIBNAME, luaopen_utf8},
 	{LUA_DBLIBNAME, luaopen_debug},
-	{LUA_BUFFERNAME, createbufferlib},
 	{nullptr, nullptr}
 };
 
