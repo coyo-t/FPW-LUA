b84b17015a4ac08be607d20fd8d78f0d495157ac
The Birthday Coyote :] <constachugga@gmail.com>
2025-01-11 02:15:55 -0800

    s

--- src/lcode.hpp
+++ src/lcode.hpp
@@ -26,19 +26,32 @@ constexpr auto NO_JUMP = -1;
 typedef enum BinOpr
 {
 	/* arithmetic operators */
-	OPR_ADD, OPR_SUB, OPR_MUL, OPR_MOD, OPR_POW,
-	OPR_DIV, OPR_IDIV,
+	OPR_ADD,
+	OPR_SUB,
+	OPR_MUL,
+	OPR_MOD,
+	OPR_POW,
+	OPR_DIV,
+	OPR_IDIV,
 	/* bitwise operators */
-	OPR_BAND, OPR_BOR, OPR_BXOR,
-	OPR_SHL, OPR_SHR,
+	OPR_BAND,
+	OPR_BOR,
+	OPR_BXOR,
+	OPR_SHL,
+	OPR_SHR,
 	/* string operator */
 	OPR_CONCAT,
 	/* comparison operators */
-	OPR_EQ, OPR_LT, OPR_LE,
-	OPR_NE, OPR_GT, OPR_GE,
+	OPR_EQ,
+	OPR_LT,
+	OPR_LE,
+	OPR_NE,
+	OPR_GT,
+	OPR_GE,
 	/* logical operators */
-	OPR_AND, OPR_OR,
-	OPR_NOBINOPR
+	OPR_AND,
+	OPR_OR,
+	OPR_NOBINOPR,
 } BinOpr;
 
 
@@ -49,7 +62,14 @@ typedef enum BinOpr
 #define luaK_codeABC(fs,o,a,b,c)	luaK_codeABCk(fs,o,a,b,c,0)
 
 
-typedef enum UnOpr { OPR_MINUS, OPR_BNOT, OPR_NOT, OPR_LEN, OPR_NOUNOPR } UnOpr;
+typedef enum UnOpr
+{
+	OPR_MINUS,
+	OPR_BNOT,
+	OPR_NOT,
+	OPR_LEN,
+	OPR_NOUNOPR,
+} UnOpr;
 
 
 /* get (pointer to) instruction of given 'expdesc' */
