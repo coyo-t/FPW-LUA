7043e3ac3bb01d86fd7fffedab412151d82c71aa
The Birthday Coyote :] <constachugga@gmail.com>
2025-01-11 04:26:36 -0800

    s

--- src/lgc.cpp
+++ src/lgc.cpp
@@ -191,12 +191,12 @@ static void clearkey(Node *n)
 static int iscleared(global_State *g, const GCObject *o)
 {
 	if (o == NULL) return 0; /* non-collectable value */
-	else if (novariant(o->tt) == LUA_TSTRING)
+	if (novariant(o->tt) == LUA_TSTRING)
 	{
 		markobject(g, o); /* strings are 'values', so are never weak */
 		return 0;
 	}
-	else return iswhite(o);
+	return iswhite(o);
 }
 
 
