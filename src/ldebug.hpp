09e35a57b931ff4e5fed611ee9447dc2004f0ca6
The Birthday Coyote :] <constachugga@gmail.com>
2025-01-11 19:55:58 -0800

    s

--- src/ldebug.hpp
+++ src/ldebug.hpp
@@ -33,29 +33,20 @@ constexpr auto ABSLINEINFO =	(-0x80);
 */
 constexpr auto MAXIWTHABS = 128;
 
-LUAI_FUNC int luaG_getfuncline (const Proto *f, int pc);
-LUAI_FUNC const char *luaG_findlocal (lua_State *L, CallInfo *ci, int n,
-                                                    StkId *pos);
-LUAI_FUNC l_noret luaG_typeerror (lua_State *L, const TValue *o,
-                                                const char *opname);
+LUAI_FUNCA luaG_getfuncline (const Proto *f, int pc) -> int;
+LUAI_FUNCA luaG_findlocal (lua_State *L, CallInfo *ci, int n, StkId *pos) -> const char*;
+LUAI_FUNC l_noret luaG_typeerror (lua_State *L, const TValue *o, const char *opname);
 LUAI_FUNC l_noret luaG_callerror (lua_State *L, const TValue *o);
-LUAI_FUNC l_noret luaG_forerror (lua_State *L, const TValue *o,
-                                               const char *what);
-LUAI_FUNC l_noret luaG_concaterror (lua_State *L, const TValue *p1,
-                                                  const TValue *p2);
-LUAI_FUNC l_noret luaG_opinterror (lua_State *L, const TValue *p1,
-                                                 const TValue *p2,
-                                                 const char *msg);
-LUAI_FUNC l_noret luaG_tointerror (lua_State *L, const TValue *p1,
-                                                 const TValue *p2);
-LUAI_FUNC l_noret luaG_ordererror (lua_State *L, const TValue *p1,
-                                                 const TValue *p2);
+LUAI_FUNC l_noret luaG_forerror (lua_State *L, const TValue *o, const char *what);
+LUAI_FUNC l_noret luaG_concaterror (lua_State *L, const TValue *p1, const TValue *p2);
+LUAI_FUNC l_noret luaG_opinterror (lua_State *L, const TValue *p1, const TValue *p2, const char *msg);
+LUAI_FUNC l_noret luaG_tointerror (lua_State *L, const TValue *p1, const TValue *p2);
+LUAI_FUNC l_noret luaG_ordererror (lua_State *L, const TValue *p1, const TValue *p2);
 LUAI_FUNC l_noret luaG_runerror (lua_State *L, const char *fmt, ...);
-LUAI_FUNC const char *luaG_addinfo (lua_State *L, const char *msg,
-                                                  TString *src, int line);
+LUAI_FUNCA luaG_addinfo (lua_State *L, const char *msg, TString *src, int line) -> const char*;
 LUAI_FUNC l_noret luaG_errormsg (lua_State *L);
-LUAI_FUNC int luaG_traceexec (lua_State *L, const Instruction *pc);
-LUAI_FUNC int luaG_tracecall (lua_State *L);
+LUAI_FUNCA luaG_traceexec (lua_State *L, const Instruction *pc) -> int;
+LUAI_FUNCA luaG_tracecall (lua_State *L) -> int;
 
 
 #endif
